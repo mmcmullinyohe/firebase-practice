847f243f234eb92094bcbf8c9c5db9d23b68d57c
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-09-09 11:03:40 -0500

    login and out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import './App.css';
 import { auth } from './firebase/init';
 import { 
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword, 
+  signOut
  } from "firebase/auth";
 
-
 function App() {
+  const [user, setUser] = React.useState({});
   function register() {
     createUserWithEmailAndPassword(auth, "email@email.com", "test123")
     .then((user) => {
@@ -20,16 +22,22 @@ function App() {
   function login() {
     signInWithEmailAndPassword(auth, "email@email.com", "test123")
        .then((user) => {
-      console.log(user)
+      setUser(user);
     })
     .catch((error) => {
       console.log(error);
     })
   }
+
+  function logout() {
+    signOut(auth);
+  }
+
   return (
     <div className='App'>
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
